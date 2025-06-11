5a4c571d98cd10b9e0a4c48065c47f63cd43f2ca
Юльян <ipon7200@gmail.com>
2025-06-11 20:59:34 +0300

    add product-list

--- my-shop/src/App.vue
+++ my-shop/src/App.vue
@@ -1,15 +1,13 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import ProductCard from './components/ProductCard.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    ProductCard
   }
 }
 </script>
